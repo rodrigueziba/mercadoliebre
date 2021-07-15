407d5fd64e22b834fb46cc46cff936c31fab7cb5
Ezequiel <chimoibarra@gmail.com>
2021-07-14 21:52:32 -0300

    mercadoliebre 2

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Servidor funcionando en el puerto 3000');
 });
 
 app.get('/', (req,res)=>{
